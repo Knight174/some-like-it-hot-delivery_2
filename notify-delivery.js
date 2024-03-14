2f77c78b0e02590d2e21855469e848415a6a9543
pinchao <cpc4253@163.com>
2024-03-14 12:07:15 +0800

    修复webhookUrl问题

--- notify-delivery.js
+++ notify-delivery.js
@@ -7,39 +7,45 @@ const IN_PROGRESS_STATUS = 'IN-PROGRESS';
 exports.handler = (event, context, cb) => {
   console.log('delivery from Step Functions received');
   if (!event.deliveryId || !event.webhookUrl || !event.deliveryAddress) {
-    let err = 'Delivery Id and/or webhookUrl and/or deliveryAddress not provided'
+    let err =
+      'Delivery Id and/or webhookUrl and/or deliveryAddress not provided';
     console.log(event);
-    cb({message: err});
+    cb({ message: err });
   }
 
-  docClient.update({
-    TableName: TABLE_NAME,
-    Key: {
-      deliveryId: event.deliveryId
-    },
-    UpdateExpression: 'set deliveryStatus = :s',
-    ExpressionAttributeValues: {
-      ':s': IN_PROGRESS_STATUS
-    },
-    ReturnValues: 'UPDATED_NEW'
-  }).promise().then(response => {
-    console.log(response);
-    let options = {
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify({
+  docClient
+    .update({
+      TableName: TABLE_NAME,
+      Key: {
         deliveryId: event.deliveryId,
-        status: IN_PROGRESS_STATUS
-      })
-    };
-    
-    return rp.post(event.webhook, options);
-  }).then(data => {
-    console.log(data);
-    cb(null, {});
-  }).catch(err => {
-    console.log(err);
-    cb({message: JSON.stringify(err)});
-  })
+      },
+      UpdateExpression: 'set deliveryStatus = :s',
+      ExpressionAttributeValues: {
+        ':s': IN_PROGRESS_STATUS,
+      },
+      ReturnValues: 'UPDATED_NEW',
+    })
+    .promise()
+    .then((response) => {
+      console.log(response);
+      let options = {
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          deliveryId: event.deliveryId,
+          status: IN_PROGRESS_STATUS,
+        }),
+      };
+
+      return rp.post(event.webhookUrl, options);
+    })
+    .then((data) => {
+      console.log(data);
+      cb(null, {});
+    })
+    .catch((err) => {
+      console.log(err);
+      cb({ message: JSON.stringify(err) });
+    });
 };
